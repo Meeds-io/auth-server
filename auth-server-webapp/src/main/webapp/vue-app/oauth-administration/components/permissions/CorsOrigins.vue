222ed256aa72f249f39436155353bf502f8f87ab
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2026-04-21 08:13:23 +0100

    feat: Rework OAuth Cors Origin URLs Admin Section UX - EXO-86177 - Meeds-io/ai#159

--- auth-server-webapp/src/main/webapp/vue-app/oauth-administration/components/permissions/CorsOrigins.vue
+++ auth-server-webapp/src/main/webapp/vue-app/oauth-administration/components/permissions/CorsOrigins.vue
@@ -18,112 +18,29 @@
 -->
 <template>
   <div>
-    <help-label
-      label="oauth.administration.clientsAllowedCorsOrigins.title"
-      label-class="text-header"
-      class="text-header"
-      tooltip="oauth.administration.clientsAllowedCorsOrigins.tooltip">
-      <template #helpContent>
-        <div class="paragraph">{{ description }}</div>
-      </template>
-    </help-label>
-    <v-tooltip :disabled="!allowAllRedirectUris" bottom>
-      <template #activator="{on, attrs}">
-        <div
-          v-on="on"
-          v-bind="attrs"
-          class="d-flex align-center mt-4 mb-0">
-          <v-switch
-            v-model="allowAll"
-            :label="$t('oauth.administration.clientsAllowedCorsOrigins.allowAny')"
-            :disabled="allowAllRedirectUris"
-            class="ma-0"
-            @change="changeAllowAll" />
-          <v-icon
-            v-if="allowAllRedirectUris"
-            size="18"
-            color="primary"
-            class="ms-2">
-            fa-info-circle
-          </v-icon>
-        </div>
-      </template>
-      <span>{{ $t('oauth.administration.clientsAllowedCorsOrigins.input.disabled') }}</span>
-    </v-tooltip>
-    <v-card
-      v-if="!allowAll"
-      max-width="min(560px, 100%)"
-      flat>
-      <v-text-field
-        v-model="uri"
-        :aria-label="$t('oauth.administration.clientsAllowedCorsOrigins.inputTitle')"
-        :placeholder="$t('oauth.administration.clientsAllowedCorsOrigins.inputPlaceholder')"
-        name="allowedOrigin"
-        class="pt-2 mb-2 border-box-sizing"
-        type="text"
-        outlined
-        dense
-        solo
-        flat
-        @click:append="addOrigin"
-        @keypress.enter="addOrigin">
-        <template #append>
-          <v-btn
-            class="me-n2"
-            icon
-            @click="addOrigin">
-            <v-icon
-              color="primary"
-              size="18">
-              fa-plus
-            </v-icon>
-          </v-btn>
+    <div class="d-flex justify-space-between">
+      <help-label
+        label="oauth.administration.clientsAllowedCorsOrigins.title"
+        tooltip="oauth.administration.clientsAllowedCorsOrigins.tooltip"
+        class="py-2">
+        <template #helpContent>
+          <div class="paragraph">{{ description }}</div>
         </template>
-      </v-text-field>
-      <v-list dense>
-        <v-tooltip
-          v-for="u in readonlyOrigins"
-          :key="u"
-          bottom>
-          <template #activator="{on, attrs}">
-            <v-list-item
-              v-on="on"
-              v-bind="attrs"
-              :title="u"
-              class="ps-0 pe-2"
-              dense>
-              <v-list-item-title class="disabled--text">
-                {{ u }}
-              </v-list-item-title>
-            </v-list-item>
-          </template>
-          <span>{{ $t('oauth.administration.clientsAllowedCorsOrigins.readonlyOrigin') }}</span>
-        </v-tooltip>
-        <v-list-item
-          v-for="u in origins"
-          :key="u"
-          :title="u"
-          class="ps-0 pe-2"
-          dense>
-          <v-list-item-title>
-            {{ u }}
-          </v-list-item-title>
-          <v-list-item-action class="my-auto">
-            <v-btn
-              :title="$t('oauth.administration.clientsAllowedCorsOrigins.delete')"
-              icon
-              @click="removeOrigin(u)">
-              <v-icon
-                color="primary"
-                size="18"
-                @click:append="removeOrigin">
-                fa-minus
-              </v-icon>
-            </v-btn>
-          </v-list-item-action>
-        </v-list-item>
-      </v-list>
-    </v-card>
+      </help-label>
+      <v-btn
+        icon
+        @click="$refs.drawer.open()">
+        <v-icon size="18">fa-edit</v-icon>
+      </v-btn>
+    </div>
+    <oauth-administration-origins-drawer
+      ref="drawer"
+      :origins="origins"
+      :allow-all-origins="allowAllOrigins"
+      :redirect-uris="redirectUris"
+      :allow-all-redirect-uris="allowAllRedirectUris"
+      :clients="clients"
+      @saved="$emit('origins-updated')" />
   </div>
 </template>
 <script>
@@ -153,7 +70,6 @@ export default {
   data: () => ({
     uri: null,
     allowAll: false,
-    originRegExp: new RegExp(/(https?:\/\/[^/]+(:\d{1,4})?)/g),
   }),
   computed: {
     description() {
@@ -200,11 +116,6 @@ export default {
     },
     removeOrigin(uri) {
       this.$emit('remove-origin', uri);
-    },
-    computeOrigin(uri) {
-      if (uri?.trim?.()?.length && this.originRegExp.test(uri)) {
-        return uri.match(this.originRegExp)?.[0];
-      }
     },
   },
 };
